85c189c931141669a5b471e732f7350e047af5ea
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-19 07:57:36 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='DCBot',
+      version='0.1.1',
+      description='Python module for interacting with Chatbase APIs',
+      url='https://github.com/github-noob-arjun/TG-DC-BOT',
+      author='Github',
+      author_email='None',
+      license='MIT Pro',
+      packages=['DCBot'],      
+      zip_safe=False)
