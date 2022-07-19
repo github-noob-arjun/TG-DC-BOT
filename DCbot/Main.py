527eb39c29baa9129b38099622aae34185800f7f
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-19 08:15:41 +0530

    Update Main.py

--- DCbot/Main.py
+++ DCbot/Main.py
@@ -12,7 +12,7 @@ DCBot = Client(
 
 @DCBot.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
-    text = START_TEXT.format(update.from_user.dc_id)
+    text = START_TEXT.format(update.from_user.mention)
     reply_markup = START_BUTTON
     await update.reply_text(
         text=text,
@@ -20,8 +20,74 @@ async def start(bot, update):
         reply_markup=reply_markup,
         quote=True
     )
+@DCBot.on_message(filters.private & filters.command(["dc"]))
+async def start(bot, update):
+    text = DC_TEXT.format(update.from_user.dc_id)
+    reply_markup = START_BUTTON
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup,
+        quote=True
+    )
+@DCBot.on_message(filters.private & filters.command(["id"]))
+async def start(bot, update):
+    text = ID_TEXT.format(update.from_user.id)
+    reply_markup = START_BUTTON
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup,
+        quote=True
+    )
+@DCBot.on_message(filters.private & filters.forwarded)
+async def info(motech, msg):
+    if msg.forward_from:
+        text = "<u>𝐅𝐨𝐫𝐰𝐚𝐫𝐝 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 👀</u> \n\n"
+        if msg.forward_from["is_bot"]:
+            text += "<u>🤖 𝐁𝐨𝐭 𝐈𝐧𝐟𝐨</u>"
+        else:
+            text += "<u>👤𝐔𝐬𝐞𝐫 𝐈𝐧𝐟𝐨</u>"
+        text += f'\n\n👨‍💼 𝐍𝐚𝐦𝐞 : {msg.forward_from["first_name"]}'
+        if msg.forward_from["username"]:
+            text += f'\n\n🔗 𝐔𝐬𝐞𝐫𝐍𝐚𝐦𝐞 : @{msg.forward_from["username"]} \n\n🆔 ID : <code>{msg.forward_from["id"]}</code>'
+        else:
+            text += f'\n\n🆔 𝐈𝐃 : `{msg.forward_from["id"]}`'
+        await msg.reply(text, quote=True)
+    else:
+        hidden = msg.forward_sender_name
+        if hidden:
+            await msg.reply(
+                f"❌️𝐄𝐫𝐫𝐨𝐫 <b><i>{hidden}</i></b> ❌️𝐄𝐫𝐫𝐨𝐫",
+                quote=True,
+            )
+        else:
+            text = f"<u>𝐅𝐨𝐫𝐰𝐚𝐫𝐝 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 👀</u>.\n\n"
+            if msg.forward_from_chat["type"] == "channel":
+                text += "<u>📢 𝐂𝐡𝐚𝐧𝐧𝐞𝐥</u>"
+            if msg.forward_from_chat["type"] == "supergroup":
+                text += "<u>🗣️ 𝐆𝐫𝐨𝐮𝐩</u>"
+            text += f'\n\n📃 𝐍𝐚𝐦𝐞 {msg.forward_from_chat["title"]}'
+            if msg.forward_from_chat["username"]:
+                text += f'\n\n➡️ 𝐅𝐫𝐨𝐦 : @{msg.forward_from_chat["username"]}'
+                text += f'\n\n🆔 𝐈𝐃 : `{msg.forward_from_chat["id"]}`'
+            else:
+                text += f'\n\n🆔 𝐈𝐃 `{msg.forward_from_chat["id"]}`\n\n'
+            await msg.reply(text, quote=True)
 
-START_TEXT = """**🆔 Your Telegram DC Is :** `{}`"""
+
+START_TEXT = """**Heyy {} This is id bot.,**
+
+**__COMMANDS__**
+
+**/id : See your Telegram ID
+/dc : See your Telegram DC
+
+• Forward any chat message to get info**
+"""
+
+DC_TEXT = """**🆔 Your Telegram DC Is :** `{}`"""
+DC_TEXT = """**🆔 Your Telegram ID Is :** `{}`"""
 START_BUTTON = InlineKeyboardMarkup(
              [[
              InlineKeyboardButton('♻️ Updates Channel ♻️', url=f"https://telegram.me/PYRO_BOTZ")
